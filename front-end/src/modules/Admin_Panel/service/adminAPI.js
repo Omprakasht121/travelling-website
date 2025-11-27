c7aa359b8144a30ac5d5bbdc1c4d49eaa3f3dc93
Omprakasht121 <omprakasht5689@gmail.com>
2025-11-28 02:01:22 +0530

    dfjnvk

--- front-end/src/modules/Admin_Panel/service/adminAPI.js
+++ front-end/src/modules/Admin_Panel/service/adminAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/content";
+const API_URL = import.meta.env.VITE_BASE_URL;
 
 // Add new
 export const addContent = async (data) => {
